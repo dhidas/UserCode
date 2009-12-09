a1be3db6aaabcb3e96330f22e15d8a0c3330133b
dhidas <>
2009-12-09 17:42:03 +0000

    oh

--- DHidasAna/Dtuple/bin/MCFM2Dtuple.cc
+++ DHidasAna/Dtuple/bin/MCFM2Dtuple.cc
@@ -33,23 +33,23 @@ int MCFM2Dtuple (TString const InFileName, TString const OutFileName)
 
 
   for (int ientry = 0; Chain.GetEntry() != 0; ++ientry) {
-    Ev.Lepton_Px[0] = Chain.GetLeaf("P4_Px")->GetValue();
-    Ev.Lepton_Py[0] = Chain.GetLeaf("P4_Py")->GetValue();
-    Ev.Lepton_Pz[0] = Chain.GetLeaf("P4_Pz")->GetValue();
-    Ev.Lepton_Pt[0] = TMath::Sqrt( TMath::Power(Chain.GetLeaf("P4_Px")->GetValue(), 2)
-                                 + TMath::Power(Chain.GetLeaf("P4_Py")->GetValue(), 2));
+    Ev.Lepton_Px[0] = Chain.GetLeaf("px4")->GetValue();
+    Ev.Lepton_Py[0] = Chain.GetLeaf("py4")->GetValue();
+    Ev.Lepton_Pz[0] = Chain.GetLeaf("pz4")->GetValue();
+    Ev.Lepton_Pt[0] = TMath::Sqrt( TMath::Power(Chain.GetLeaf("px4")->GetValue(), 2)
+                                 + TMath::Power(Chain.GetLeaf("py4")->GetValue(), 2));
 
-    Ev.Photon_Px[0] = Chain.GetLeaf("P5_Px")->GetValue();
-    Ev.Photon_Py[0] = Chain.GetLeaf("P5_Py")->GetValue();
-    Ev.Photon_Pz[0] = Chain.GetLeaf("P5_Pz")->GetValue();
-    Ev.Photon_Pt[0] = TMath::Sqrt( TMath::Power(Chain.GetLeaf("P5_Px")->GetValue(), 2)
-                                 + TMath::Power(Chain.GetLeaf("P5_Py")->GetValue(), 2));
+    Ev.Photon_Px[0] = Chain.GetLeaf("px5")->GetValue();
+    Ev.Photon_Py[0] = Chain.GetLeaf("py5")->GetValue();
+    Ev.Photon_Pz[0] = Chain.GetLeaf("pz5")->GetValue();
+    Ev.Photon_Pt[0] = TMath::Sqrt( TMath::Power(Chain.GetLeaf("px5")->GetValue(), 2)
+                                 + TMath::Power(Chain.GetLeaf("py5")->GetValue(), 2));
 
     Ev.Run = 0;
     Ev.Event = ientry;
-    Ev.MetMag = TMath::Sqrt( TMath::Power(Chain.GetLeaf("P3_Px")->GetValue(), 2)
-                           + TMath::Power(Chain.GetLeaf("P3_Py")->GetValue(), 2));
-    Ev.MetPhi = TVector2(Chain.GetLeaf("P3_Px")->GetValue(), Chain.GetLeaf("P3_Px")->GetValue()).Phi();
+    Ev.MetMag = TMath::Sqrt( TMath::Power(Chain.GetLeaf("px3")->GetValue(), 2)
+                           + TMath::Power(Chain.GetLeaf("py3")->GetValue(), 2));
+    Ev.MetPhi = TVector2(Chain.GetLeaf("px3")->GetValue(), Chain.GetLeaf("px3")->GetValue()).Phi();
     Ev.SumEt = Ev.Lepton_Pt[0] + Ev.Photon_Pt[0];
 
     DT.Fill();
