9febd7949ae03226a7551bd4e4f4c14798cd8be8
dhidas <>
2010-08-23 21:29:07 +0000

    better plotting talk THAT

--- scripts/LatexPlots.pl
+++ scripts/LatexPlots.pl
@@ -1,15 +1,42 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+
+my $TEMPFILENAME = "Plots";
+
+my @EPS;
 
 
-my $DIR = $ARGV[0];
-print "Directory: $DIR\n";
 
-my @EPS = `ls $DIR/\*.eps`;
-
-foreach (@EPS) {
-  chomp;
-  print "\\includegraphics[width=0.5\\linewidth]{$_}\n";
+if (-e $TEMPFILENAME.".tex") {
+  die "$TEMPFILENAME already exists.  move it first";
 }
 
+open MYFILE, ">$TEMPFILENAME.tex";
+
+print MYFILE "\\documentclass[prl]{revtex4}\n";
+print MYFILE "\\usepackage{graphicx}\n";
+print MYFILE "\\begin{document}\n\n";
+
+foreach (@ARGV) {
+  print "Directory: $_\n";
+  @EPS = `ls $_/\*.eps`;
+
+  s/_/\\_/g;
+  print MYFILE "\\section{$_}\n";
+  foreach (@EPS) {
+    chomp;
+    print MYFILE "\\includegraphics[width=0.5\\linewidth]{$_}\n";
+  }
+}
+
+
+
+print MYFILE "\\end{document}";
+close MYFILE;
+
+`latex $TEMPFILENAME.tex`;
+`latex $TEMPFILENAME.tex`;
+`dvips -t letter -Ppdf -G0 -tletter $TEMPFILENAME.dvi -o $TEMPFILENAME.ps`;
+`ps2pdf $TEMPFILENAME.ps`;
+`rm $TEMPFILENAME.ps $TEMPFILENAME.dvi $TEMPFILENAME.tex $TEMPFILENAME.log $TEMPFILENAME.aux`;
 
 
