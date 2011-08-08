86289ee816987d766170608919b0488e41538b7a
dhidas <>
2011-08-08 12:44:18 +0000

    changeme

---
+++ MultiJets/MultiJetsRooStats/bin/GetHistFromCan.cc
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////
+//
+// Dean Andrew Hidas <Dean.Andrew.Hidas@cern.ch>
+//
+// Created on: Mon Aug  8 14:38:24 CEST 2011
+//
+////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TH1F.h"
+#include "TString.h"
+
+
+int GetHistFromCan (TString const InFileName, TString const OutFileName)
+{
+  TFile fin(InFileName, "read");
+  TCanvas* c = (TCanvas*) fin.Get("c2");
+  TH1F* hist = (TH1F*) c->FindObject("Mjjj_70_20_160_6jet");
+
+  TFile fout(OutFileName, "recreate");
+  TH1F* newhist = (TH1F*) hist->Clone();
+  newhist->SetDirectory(&fout);
+  fout.Write();
+  fout.Close();
+  fin.Close();
+
+  return 0;
+}
+
+
+int main (int argc, char* argv[])
+{
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " [InFileName] [OutFileName]" << std::endl;
+    return 1;
+  }
+
+  GetHistFromCan(argv[1], argv[2]);
+
+  return 0;
+}
