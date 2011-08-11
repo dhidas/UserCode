dc526a443782feb07651f0bacfb728d0c5e1953c
dhidas <>
2011-08-11 09:56:06 +0000

    lpc pe update

--- MultiJets/MultiJetsRooStats/submit_lpcfarm.sh
+++ MultiJets/MultiJetsRooStats/submit_lpcfarm.sh
@@ -10,13 +10,32 @@ eval `scramv1 runtime -sh`
 cd -
 
 OutDir=$1
+InFileName=data6jetpt70_160offset_1089pb3fits10GeVBins.root
 
 mkdir -pv $OutDir/Log $OutDir
 
 cp -v run_lpcfarm.sh $OutDir
 cp -v RunRooStats3Param $OutDir
-cp -v Data/DijetMassFit_data_881pb-1_6jets_pt70.root $OutDir
-cp -v submit_lpcfarm.jdl $OutDir
+cp -v Data/$InFileName $OutDir
+
+
+cat > $OutDir/submit_lpcfarm.jdl << +EOF
+OutDir = ./
+Executable = run_lpcfarm.sh
+
+universe = vanilla
+Requirements = Memory >= 199 &&OpSys == "LINUX"&& (Arch != "DUMMY" )&& Disk > 1000000
+Should_Transfer_Files = YES
+WhenToTransferOutput = ON_EXIT
+Transfer_Input_Files = $InFileName, RunRooStats3Param
+Output = \$(OutDir)/Log/Log_\$(Process).out
+Error =  \$(OutDir)/Log/Log_\$(Process).err
+Log =    \$(OutDir)/Log/Log_\$(Process).log
+notify_user = dhidas@FNAL.GOV
+Arguments = \$(Process)
+Queue 500
++EOF
+
 
 cd $OutDir
 condor_submit submit_lpcfarm.jdl
