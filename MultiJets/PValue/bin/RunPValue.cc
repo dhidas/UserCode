bcf873f57871768ed69b9ba65723d4abe72eb6d2
dhidas <>
2011-08-31 11:34:20 +0000

    I need an electric

--- MultiJets/PValue/bin/RunPValue.cc
+++ MultiJets/PValue/bin/RunPValue.cc
@@ -26,30 +26,46 @@
 // I'm using these as globals just for ease.
 TH1F* hToFit;  // Histogram to fit
 TF1* fToFit;   // Function used in fitting
-float ParC[5]; // Parameter central value
-float ParE[5]; // Parameter error
+
+int const NPAR = 6;
+float ParC[NPAR]; // Parameter central value
+float ParE[NPAR]; // Parameter error
+
+float const XMIN     =  230;
+float const XMAX     = 1520;
+float const BINWIDTH = 10;
+int   const NBINS    = (XMAX - XMIN) / BINWIDTH;
 
 
 
-
-TH1F* GetPEExpo (float const inN, float const incexp, int const ipe,  bool const DoSyst)
+TH1F* GetPE (float const inN, std::vector< std::pair<float, float> >& InPar, int const ipe,  bool const DoSyst)
 {
   // This function gets a PE in any way you tell it to!  ha!
 
-  // Systematics in PEs or not??
-  float const cexp = DoSyst ? incexp * (1. + 0.05 * gRandom->Gaus(0, 1)) : incexp;
-  int   const N    = DoSyst ? gRandom->Poisson(inN * (1. + 0.03 * gRandom->Gaus(0, 1))) : gRandom->Poisson(inN);
-  printf("PE cexp/N: %12E  %9i\n", cexp, N);
+  // Parameters for PE, with or without syst
+  std::vector<float> Par;
+  for (size_t i = 0; i != InPar.size(); ++i) {
+    if (DoSyst) {
+      Par.push_back( InPar[i].first * (1. + InPar[i].second * gRandom->Gaus(0, 1)) );
+    } else {
+      Par.push_back(InPar[i].first);
+    }
+  }
 
+
+  // Systematics in PEs or not??
+  int   const N    = DoSyst ? gRandom->Poisson(inN * (1. + 0.03 * gRandom->Gaus(0, 1))) : gRandom->Poisson(inN);
 
   // New histogram we'll return later on..  you will own this, not me!!
-  TH1F* h = new TH1F("PE", "PE", 65, 170, 800);
+  TH1F* h = new TH1F("PE", "PE", NBINS, XMIN, XMAX);
 
   // Define normalized BG function in the range
-  TF1 f("myexp", "[0]*TMath::Exp([1]*x)", 170, 800);
+  TF1 f("myexp", "[0]*(((1.-x/7000.)^[1])/((x/7000.)^[2]))", XMIN, XMAX);
+  for (size_t i = 0; i != Par.size(); ++i) {
+    f.SetParameter(i, Par[i]);
+  }
   f.SetParameter(0, 1);
-  f.SetParameter(1, cexp);
-  f.SetParameter(0, 1.0/f.Integral(170,800));
+  f.SetParameter(0, 1.0 / f.Integral(XMIN, XMAX));
 
   // Fill the new histogram based on the BG pdf defined above
   for (int i = 0; i < N; ++i) {
@@ -58,12 +74,14 @@ TH1F* GetPEExpo (float const inN, float const incexp, int const ipe,  bool const
 
   // This is a test so that I know that I have the normalizaton correct
   // In general, don't run this..
-  if (true) {
+  if (false) {
     // Define the function..
-    TF1 ff("myexp1", "[0]*TMath::Exp([1]*x)", 170, 800);
+    TF1 ff("myexp1", "[0]*(((1.-x/7000.)^[1])/((x/7000.)^[2]))", XMIN, XMAX);
+    for (size_t i = 0; i != Par.size(); ++i) {
+      ff.SetParameter(i, Par[i]);
+    }
     ff.SetParameter(0, 1);
-    ff.SetParameter(1, cexp);
-    ff.SetParameter(0, 10*N/ff.Integral(170,800));
+    ff.SetParameter(0, BINWIDTH * N / ff.Integral(XMIN, XMAX));
 
     // Plot it and the PE
     TCanvas c;
@@ -80,15 +98,50 @@ TH1F* GetPEExpo (float const inN, float const incexp, int const ipe,  bool const
 }
 
 
+float GetACCERROR (float const Mjjj)
+{
+  // 250    0.203786
+  // 300    0.175059
+  // 350    0.127826
+  // 400    0.136186
+  // 450    0.140467
+  // 500    0.171287
+  // 750    0.212378
+  //1250    0.335977
+  //1500    0.565487
+
+  // Fixed 250-500 to the 250 val and fit pol3. might undershoot around 1000, but ain't too bad
+  //p0                        =     0.140834   +/-   0.0160314   
+  //p1                        =  0.000392742   +/-   7.3481e-05  
+  //p2                        = -7.49412e-07   +/-   9.60659e-08 
+  //p3                        =  4.50564e-10   +/-   3.69923e-11 
+
+  return 0.140834 + 0.000392742*Mjjj - -7.49412e-07*Mjjj*Mjjj + 4.50564e-10*Mjjj*Mjjj*Mjjj;
+}
 
 
 
 float GetAcceptanceForMjjj (float const Mjjj)
 {
-  // Get the acceptance for a given mass.  Thanks Dan!
+  // Get the acceptance for a given mass
+  //TF1* f = Accept->GetFunction("fit1");
+  //f->GetParameter(0)
+  //f->GetParameter(1)
+  //f->GetParameter(2)
+  //2nd degree polynomial numbers:
+  //p0                        =   -0.0173967   +/-   0.000576407 
+  //p1                        =  8.54121e-05   +/-   2.28888e-06 
+  //p2                        = -2.44194e-08   +/-   1.63146e-09 
+  //
+  //3rd degree polynomial numbers:
+  //p0                        =     -0.01027   +/-   0.00171542  
+  //p1                        =  4.38331e-05   +/-   1.08208e-05 
+  //p2                        =  4.43791e-08   +/-   1.96505e-08 
+  //p3                        = -3.69426e-11   +/-   9.06989e-12
 
-  // Hell of a lot faster
-  return -5.56179969055969892e-03 - 4.01623842089755741e-06 * Mjjj + 2.49580149009780901e-07 * Mjjj * Mjjj;
+
+  return -0.01027 + 4.38331e-05*Mjjj + 4.43791e-08*Mjjj*Mjjj - 3.69426e-11*Mjjj*Mjjj*Mjjj;
+  //return -0.0173967 + 8.54121e-05 * Mjjj + -2.44194e-08 * Mjjj * Mjjj;
 
 }
 
@@ -97,11 +150,7 @@ float GetAcceptanceForMjjj (float const Mjjj)
 std::pair<float, float> GetGausWidthRange (float const Mjjj)
 {
   // Get the range for the gaussian width you want to use for a given Mjjj
-  // This will eventually be a parametrization
-
-  if (Mjjj < 250) return std::make_pair<float, float>(10, 15);
-  if (Mjjj < 350) return std::make_pair<float, float>(10 + 10 * (Mjjj - 250.) / 100., 15 + 10 * (Mjjj - 250.) / 100.);
-  return std::make_pair<float, float>(20, 25);
+  return std::make_pair<float, float>(Mjjj * 0.065, Mjjj * 0.075);
 }
 
 template <class T> double KahanSummation(T begin, T end)
@@ -165,19 +214,17 @@ void NegativeLogLikelihood (int& NParameters, double* gin, double& f, double* Pa
   // is if one should go beyond this
 
   // You should not print anything in this function save for debug time
-  //for (int i = 0; i != 5; ++i) {
+  //for (int i = 0; i != 6; ++i) {
   //  printf("Par: %i %7E %7E %7E\n", i, Par[i], ParC[i], ParE[i]);
   //}
 
 
   // These are the parameters of the fit...
-  // ("fSigBG","[0]*TMath::Gaus(x, [1], [2], 1) + [3]*TMath::Exp([4]*x)",  170, 800);
+  // ("fSigBG","[0]*TMath::Gaus(x, [1], [2], 1) + [3]*(((1.-x/7000.)^[4])/((x/7000.)^[5]))",  170, 800);
   // Systematics: start on the 5.  should be gaussian centered at 0 width one
-  fToFit->SetParameter(0, Par[0]);
-  fToFit->SetParameter(1, Par[1]);
-  fToFit->SetParameter(2, Par[2]);
-  fToFit->SetParameter(3, Par[3]);
-  fToFit->SetParameter(4, Par[4]);
+  for (int i = 0; i != NPAR; ++i) {
+    fToFit->SetParameter(i, Par[i]);
+  }
 
   // LL variable
   static long double LogLikelihood;
@@ -185,12 +232,12 @@ void NegativeLogLikelihood (int& NParameters, double* gin, double& f, double* Pa
   static long double mu = 0.0;
 
   // Loop over all bins in histogram
-  static int const iStart = hToFit->FindBin(170);
-  static int const iStop  = hToFit->FindBin(800);
+  static int const iStart = hToFit->FindBin(XMIN);
+  static int const iStop  = hToFit->FindBin(XMAX);
   for (int ibinX = iStart; ibinX <= iStop; ++ibinX) {
 
     mu = fToFit->Integral( hToFit->GetBinLowEdge(ibinX), hToFit->GetBinLowEdge(ibinX) + hToFit->GetBinWidth(ibinX)) / hToFit->GetBinWidth(ibinX);
-    //printf("Bin X mu: %4i %9.1f %9E\n", ibinX, hToFit->GetBinLowEdge(ibinX), mu);
+    //printf("Bin X-X mu: %4i %9.1f-%9.1f %12.3E\n", ibinX, hToFit->GetBinLowEdge(ibinX), hToFit->GetBinLowEdge(ibinX) + hToFit->GetBinWidth(ibinX), (float) mu);
 
     if (mu > 0.1) {
       LogLikelihood += (hToFit->GetBinContent(ibinX) * TMath::Log(mu)
@@ -198,7 +245,7 @@ void NegativeLogLikelihood (int& NParameters, double* gin, double& f, double* Pa
     }
   }
 
-  for (int i = 0; i < 5; ++i) {
+  for (int i = 0; i < NParameters; ++i) {
     if (ParE[i] != -999) {
       LogLikelihood -= TMath::Power((Par[i] - ParC[i])/ParE[i], 2);
     }
@@ -220,15 +267,12 @@ float MinimizeNLL (int const Section, int const ipe, float const SignalMass, TF1
 {
   // This function will setup Minuit and call the minimization routine.
 
-  // Number of minuit parameters
-  int const NParams = 5;
-
   // These are for Minuit
   double ArgList[10];
   int ErrorFlag;
 
   // Grab a new instance of Minuit with NParams
-  TMinuit MyMinuit(NParams);
+  TMinuit MyMinuit(NPAR);
 
   // Set the printlevel -2 minimum, higher for more junk
   MyMinuit.SetPrintLevel(-2);
@@ -237,41 +281,49 @@ float MinimizeNLL (int const Section, int const ipe, float const SignalMass, TF1
   // NOTE: You should always stay AWAY from the limits... ie the fit should NOT
   // hit the limit
   std::pair<float, float> gWidth = GetGausWidthRange(SignalMass);
-  float const NBG = TMath::Exp(fFunc->GetParameter(0));
-  float const NBG_min = 0;
-  float const NBG_max = 1000;
-  float const BGExp = fFunc->GetParameter(1);
-  float const BGExp_min = -0.1;
-  float const BGExp_max =  0;
+  float const p3     = fFunc->GetParameter(0);
+  float const p3_min = 0;
+  float const p3_max = 6000;
+  float const p4     = fFunc->GetParameter(1);
+  float const p4_min =    0;
+  float const p4_max =  100;
+  float const p5     = fFunc->GetParameter(2);
+  float const p5_min = -2;
+  float const p5_max =  0;
+
   // Errors...
-  float const eNBG = NBG * 0.03;
-  float const eBGExp = fFunc->GetParError(1);
+  float const ep3 = ep3 * 0.03;
+  //float const ep4 = fFunc->GetParError(4);
+  //float const ep5 = fFunc->GetParError(5);
 
 
   // This is the function we're working with
-  // TF1 ("fSigBG","[0]*TMath::Gaus(x, [1], [2], 1) + [3]*TMath::Exp([4]*x)",  170, 800);
+  // ("fSigBG","[0]*TMath::Gaus(x, [1], [2], 1) + [3]*(((1.-x/7000.)^[4])/((x/7000.)^[5]))",  170, 800);
   if (IsBGOnly) {
     // This is for BG only fits
     ParC[0] = -999;
     ParC[1] = SignalMass;
     ParC[2] = (gWidth.first+gWidth.second)/2;
-    ParC[3] = NBG;
-    ParC[4] = BGExp;
+    ParC[3] = fFunc->GetParameter(0);
+    ParC[4] = fFunc->GetParameter(1);
+    ParC[5] = fFunc->GetParameter(2);
 
     ParE[0] = -999;
     ParE[1] = -999;
     ParE[2] = -999;
-    ParE[3] = eNBG;
-    ParE[4] = eBGExp;
+    ParE[3] = fFunc->GetParError(0);
+    ParE[4] = fFunc->GetParError(1);
+    ParE[5] = fFunc->GetParError(2);
     if (!DoSyst) {
       ParE[3] = -999;
       ParE[4] = -999;
+      ParE[5] = -999;
 
-      int NPar = NParams;
-      double Pars[5] = {ParC[0], ParC[1], ParC[2], ParC[3], ParC[4]};
+      double Pars[NPAR] = {ParC[0], ParC[1], ParC[2], ParC[3], ParC[4], ParC[5]};
       double *Blank;
       double NLL = 0;
 
+      int NPar = NPAR;
       NegativeLogLikelihood(NPar, Blank, NLL, Pars, 0);
       return NLL;
     }
@@ -279,42 +331,49 @@ float MinimizeNLL (int const Section, int const ipe, float const SignalMass, TF1
     MyMinuit.DefineParameter(0, "GausNorm", 0, 0.001, 0, 1000);
     MyMinuit.DefineParameter(1, "GausMean",  SignalMass, 0.01, SignalMass-1, SignalMass+1);
     MyMinuit.DefineParameter(2, "GausWidth", (gWidth.first+gWidth.second)/2., 0.001, gWidth.first, gWidth.second);
-    MyMinuit.DefineParameter(3, "BGNorm", NBG, 0.001, NBG_min, NBG_max);
-    MyMinuit.DefineParameter(4, "BGExp", BGExp, 0.000001, BGExp_min, BGExp_max);
+    MyMinuit.DefineParameter(3, "BGp3", p3, 0.001, p3_min, p3_max);
+    MyMinuit.DefineParameter(4, "BGp4", p4, 0.000001, p4_min, p4_max);
+    MyMinuit.DefineParameter(5, "BGp5", p5, 0.000001, p5_min, p5_max);
     MyMinuit.FixParameter(0);
     MyMinuit.FixParameter(1);
     MyMinuit.FixParameter(2);
     if (!DoSyst) {
       MyMinuit.FixParameter(3);
       MyMinuit.FixParameter(4);
+      MyMinuit.FixParameter(5);
     }
   } else {
     // This is for Signal+BG fits
     ParC[0] = -999;
     ParC[1] = SignalMass;
     ParC[2] = (gWidth.first+gWidth.second)/2;
-    ParC[3] = NBG;
-    ParC[4] = BGExp;
+    ParC[3] = fFunc->GetParameter(0);
+    ParC[4] = fFunc->GetParameter(1);
+    ParC[5] = fFunc->GetParameter(2);
 
     ParE[0] = -999;
     ParE[1] = -999;
     ParE[2] = -999;
-    ParE[3] = eNBG;
-    ParE[4] = eBGExp;
+    ParE[3] = fFunc->GetParError(0);
+    ParE[4] = fFunc->GetParError(1);
+    ParE[5] = fFunc->GetParError(2);
     if (!DoSyst) {
       ParE[3] = -999;
       ParE[4] = -999;
+      ParE[5] = -999;
     }
 
     MyMinuit.DefineParameter(0, "GausNorm", 0, 0.001, 0, 1000);
-    MyMinuit.DefineParameter(1, "GausMean",  SignalMass, 0.01, SignalMass-1, SignalMass+1);
-    MyMinuit.DefineParameter(2, "GausWidth", (gWidth.first+gWidth.second)/2., 0.001, gWidth.first, gWidth.second);
-    MyMinuit.DefineParameter(3, "BGNorm", NBG, 0.001, NBG_min, NBG_max);
-    MyMinuit.DefineParameter(4, "BGExp", BGExp, 0.000001, BGExp_min, BGExp_max);
+    MyMinuit.DefineParameter(1, "GausMean",  SignalMass, 0.1, SignalMass-1, SignalMass+1);
+    MyMinuit.DefineParameter(2, "GausWidth", (gWidth.first+gWidth.second)/2., 0.01, gWidth.first, gWidth.second);
+    MyMinuit.DefineParameter(3, "BGp3", p3, 0.1, p3_min, p3_max);
+    MyMinuit.DefineParameter(4, "BGp4", p4, 0.01, p4_min, p4_max);
+    MyMinuit.DefineParameter(5, "BGp5", p5, 0.00001, p5_min, p5_max);
     MyMinuit.FixParameter(1);
     if (!DoSyst) {
       MyMinuit.FixParameter(3);
       MyMinuit.FixParameter(4);
+      MyMinuit.FixParameter(5);
     }
 
   }
@@ -332,7 +391,7 @@ float MinimizeNLL (int const Section, int const ipe, float const SignalMass, TF1
 
   // Set the function to minimize and maximum number of iterations
   MyMinuit.SetFCN(NegativeLogLikelihood);
-  MyMinuit.SetMaxIterations(1000);
+  MyMinuit.SetMaxIterations(10000);
 
   // Actual call to do minimimazation
   MyMinuit.Migrad();
@@ -354,8 +413,9 @@ float MinimizeNLL (int const Section, int const ipe, float const SignalMass, TF1
       sprintf(BUFF, "Fit_Data_BG.eps");
     }
     TCanvas c;
-    hToFit->SetAxisRange(170, 800, "X");
+    hToFit->SetAxisRange(XMIN, XMAX, "X");
     hToFit->Draw("hist");
+
     fToFit->Draw("same");
     c.SaveAs(BUFF);
   }
@@ -365,8 +425,8 @@ float MinimizeNLL (int const Section, int const ipe, float const SignalMass, TF1
   // MyMinuit.mnmnos();
 
   // Grab parameters from minuit
-  double OPar[NParams], OEPar[NParams];
-  for (int i = 0; i < NParams; ++i) {
+  double OPar[NPAR], OEPar[NPAR];
+  for (int i = 0; i < NPAR; ++i) {
     MyMinuit.GetParameter(i, OPar[i], OEPar[i]);
     printf("Fitted Param: %i %9E  +/-  %9E\n", i, OPar[i], OEPar[i]);
   }
@@ -375,7 +435,7 @@ float MinimizeNLL (int const Section, int const ipe, float const SignalMass, TF1
   // For the parameters which minimize the NLL, lets get the NLL value and return it
   double NLL = 0;
   double* Blank;
-  int NPar = NParams;
+  int NPar = NPAR;
   NegativeLogLikelihood(NPar, Blank, NLL, OPar, 0);
 
   return NLL;
@@ -404,13 +464,13 @@ int RunPValue (TString const InFileName, int const Section, bool const DoSyst)
 {
   // Some basic parameters
   float const StepSize    =  10;
-  float const BeginMass   = 200;
-  float const EndMass     = 500;
-  int   const NPerSection =  50;
+  float const BeginMass   =  250;
+  float const EndMass     = 1500;
+  int   const NPerSection =   50;
 
 
   // Set the randome seed based on section number
-  gRandom->SetSeed(943437 * (Section + 2));
+  gRandom->SetSeed(12332 * (Section + 2));
 
 
   // Setup output file
@@ -439,17 +499,15 @@ int RunPValue (TString const InFileName, int const Section, bool const DoSyst)
     std::cerr << "ERROR: cannot open input file: " << InFileName << std::endl;
     exit(1);
   }
-  TH1F* DataTH1F = (TH1F*) InFile.Get("Mjjj_45_20_130_6jet");
-  TF1* fFunc = DataTH1F->GetFunction("fit1");
-  //TH1F* DataTH1F = (TH1F*) InFile.Get("DataBkgSubTracked_IntValues");
-  //TF1* fFunc = DataTH1F->GetFunction("fit3");
+  TH1F* DataTH1F = (TH1F*) InFile.Get("Mjjj_70_20_160_6jet");
+  TF1* fFunc = DataTH1F->GetFunction("g3");
 
   // Number of events from data
-  int const NFromData = (int) DataTH1F->Integral(17, 80);
+  //int const NFromData = (int) DataTH1F->Integral(23, 152);
 
   // Set the functino to fit.  Be VERY careful if oyu change this... you need to change a lot
   // of other things too...
-  fToFit = new TF1("FitFunction", "[0]*TMath::Gaus(x, [1], [2], 1) + [3]*TMath::Exp([4]*x)", 170, 800);
+  fToFit = new TF1("FitFunction", "[0]*TMath::Gaus(x, [1], [2], 1) + [3]*(((1.-x/7000.)^[4])/((x/7000.)^[5]))", XMIN, XMAX);
 
   if (Section == -1) {
     // Do some Data Fit
@@ -459,7 +517,7 @@ int RunPValue (TString const InFileName, int const Section, bool const DoSyst)
     hToFit = DataClone;
 
     // Grab the background only LL
-    float const BGLL = -1.0 * MinimizeNLL(Section, -1, -999, fFunc, true, DoSyst);
+    float const BGLL = 123;//-1.0 * MinimizeNLL(Section, -1, -999, fFunc, true, DoSyst);
     if (BGLL == -9999) {
       std::cerr << "ERROR: bad BG only fit for data.  This is a real problem..." << std::endl;
       throw;
@@ -489,11 +547,17 @@ int RunPValue (TString const InFileName, int const Section, bool const DoSyst)
 
       // Number in PE
       //float const NPE = (float) NFromData;
-      float const NPE = fFunc->Integral(170, 800) / 10.;
+      float const NPE = fFunc->Integral(XMIN, XMAX) / BINWIDTH;
       printf("iep: %i NPE: %12.2f\n", ipe, NPE);
 
+      // Set parameters and errors
+      std::vector< std::pair<float, float> > ParAndE;
+      for (int ipar = 0; ipar != NPAR; ++ipar) {
+        ParAndE.push_back( std::make_pair<float, float>(fFunc->GetParameter(ipar), fFunc->GetParError(ipar)) );
+      }
+
       // Get PE
-      hToFit = GetPEExpo(NPE, fFunc->GetParameter(1), ipe, DoSyst);
+      hToFit = GetPE(NPE, ParAndE, ipe, DoSyst);
 
       // Grab the background only LL
       float const BGLL = -1.0 * MinimizeNLL(Section, ipe, -999, fFunc, true, DoSyst);
@@ -536,18 +600,13 @@ int RunPValue (TString const InFileName, int const Section, bool const DoSyst)
 
 int main (int argc, char* argv[])
 {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " [Section]" << std::endl;
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " [InFile] [Section]" << std::endl;
     return 1;
   }
 
-  //TString const InFileName = "/uscms/home/dhidas/Data35pb/ExpoFit_data_35pb-1_6jets_and_scaled_4jets_pt45.root";
-  TString const InFileName = "./plotsForDean.root";
-  //TString const InFileName = "/uscms/home/dhidas/Data35pb/ExpoFit_data_35pb-1_6jets_and_scaled_4jets_pt45.root";
-  //TString const InFileName = "/Users/dhidas/Data35pb/ExpoFit_data_35pb-1_6jets_and_scaled_4jets_pt45.root";
-  //TString const InFileName = "/users/h2/dhidas/Data35pb/ExpFit_data_35pb-1_6jets_and_scaled_4jets_pt45.root";
-
-  int const Section = atoi(argv[1]);
+  TString const InFileName = argv[1];
+  int const Section = atoi(argv[2]);
 
   // Run Systematics?
   bool const DoSyst = false;
