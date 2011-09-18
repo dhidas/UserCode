652d57548503ec59e59b11c431386c6b966cc19a
dhidas <>
2011-09-18 12:51:59 +0000

    for cls on hexfarm

---
+++ MultiJets/MultiJetsRooStats/run_hexfarm_cls.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo 'Hello World!'
+echo 'Section: ' $1
+
+
+source /osg/apps/osg/setup.sh
+
+export PATH=/home/cdfcaf/condor/dist/bin:${PATH}
+
+export VO_CMS_SW_DIR=/cms/base/cmssoft
+export SCRAM_ARCH=slc5_amd64_gcc434
+source $VO_CMS_SW_DIR/cmsset_default.sh
+
+
+export ROOTSYS=/cms/data24/dhidas/CernRoot/root_2011.09.17_HEAD/root
+export LD_LIBRARY_PATH=$ROOTSYS/lib
+PATH=$PATH:$ROOTSYS/bin
+
+
+
+
+EXE=$PWD/RunRooCLS
+DAT=$PWD/dataGE6jets_2177pb_10GeVBins_Hist.root
+
+echo "HOSTNAME " $HOSTNAME
+
+echo "$EXE $DAT -1 $1"
+$EXE $DAT -1 $1
+
+
---
+++ MultiJets/MultiJetsRooStats/submit_hexfarm_cls.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+echo 'Submitting to hexfarm!'
+
+ThisDir=$PWD
+
+export VO_CMS_SW_DIR=/cms/base/cmssoft
+export SCRAM_ARCH=slc5_amd64_gcc434
+source $VO_CMS_SW_DIR/cmsset_default.sh
+
+source /osg/apps/osg/setup.sh
+
+export PATH=/home/cdfcaf/condor/dist/bin:${PATH}
+
+
+
+
+OutDir=$1
+InFileName=dataGE6jets_2177pb_10GeVBins_Hist.root
+
+mkdir -pv $OutDir/Log $OutDir
+
+cp -v run_hexfarm_cls.sh $OutDir
+cp -v RunRooCLS $OutDir
+cp -v Data/$InFileName $OutDir
+
+
+cat > $OutDir/submit_hexfarm_cls.jdl << +EOF
+OutDir = ./
+Executable = run_hexfarm_cls.sh
+
+universe = vanilla
+Should_Transfer_Files = NO
+Output = \$(OutDir)/Log/Log_\$(Process).out
+Error =  \$(OutDir)/Log/Log_\$(Process).err
+Log =    \$(OutDir)/Log/Log_\$(Process).log
+notify_user = dhidas@FNAL.GOV
+Arguments = \$(Process)
+Queue 126
++EOF
+
+
+
+cd $OutDir
+condor_submit submit_hexfarm_cls.jdl
+cd $ThisDir
